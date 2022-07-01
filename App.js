fb0bded7f4688617278b0c61569f6c7a44e01d79
Leandreal <leandrelim@gmail.com>
2022-07-01 22:27:37 +0800

    Add Colour Button

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
+
+
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+
+function renderItem ({item}) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`,
+      },
+    ]);
+  }
+ 
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+        >
+
+        <Text style={{ color: "red" }}>Add colour</Text>
+      </TouchableOpacity>
+
+
+      <FlatList 
+      data={colorArray} 
+      renderItem={renderItem} 
+      style= {{ width: "100%" }}
+     />
     </View>
   );
  
