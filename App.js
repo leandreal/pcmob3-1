014b6cc8397ea8f94b2907bbd4f75e786cc827ef
Leandreal <leandrelim@gmail.com>
2022-07-05 22:16:27 +0800

    Added useEffect (Add Colour) and Changed Flatlist to 4 columns.

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Button, Image, Dimensions } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 
+  
 function HomeScreen({ navigation }) {
   const [colorArray, setColorArray] = useState([]);
- 
-function renderItem({ item }) {
+
+
+  
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button onPress={addColor} title="Add Color" />,
+    });
+  })
+
+
+  function renderItem({ item }) {
     return (
       <TouchableOpacity
-        onPress={() => navigation.navigate("DetailsScreen", { ...item })}
+        onPress={() => navigation.navigate("Details", item)}
       >
         <BlockRGB red={item.red} green={item.green} blue={item.blue} />
       </TouchableOpacity>
     );
   }
 
-  
-
-function addColor() {
+  function addColor() {
     setColorArray([
       ...colorArray,
       {
@@ -33,21 +41,16 @@ function addColor() {
     ]);
   }
 
-
-function resetColor() {
+  function resetColor() {
     setColorArray([]);
   }
- 
+  
+
   return (
     <View style={styles.container}>
-      
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={addColor}
-        >
 
-        <Text style={{ color: "blue" }}>Add colour</Text>
-      </TouchableOpacity>
+
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} numColumns={4} />
 
       <TouchableOpacity
         style={styles.resetButton}
@@ -57,25 +60,24 @@ function resetColor() {
         <Text style={{ color: "green" }}>Reset colour</Text>
       </TouchableOpacity>
 
-
-      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
-  );
+ 
+  )}
 
-}
 
+  
 
 function DetailsScreen({ route }) {
-    // Destructure this object so we don't have to type route.params.red etc
     const { red, green, blue } = route.params;
    
     return (
       <View
-        style={[
-          styles.container,
-          { backgroundColor: `rgb(${red}, ${green}, ${blue})` },
-        ]}
+      style={[
+        styles.container,
+        { backgroundColor: `rgb(${red}, ${green}, ${blue})` },
+      ]} 
       >
+
         <View style={{ padding: 30 }}>
           <Text style={styles.detailText}>Red: {red}</Text>
           <Text style={styles.detailText}>Green: {green}</Text>
@@ -94,7 +96,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Colour List" component={HomeScreen} />
-        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -111,25 +113,19 @@ const styles = StyleSheet.create({
 
   list: {
     width: "100%",
+    flex: 1, 
+   
+  
   },
 
   detailText: {
     fontSize: 24,
     marginBottom: 20,
+    color: "white",
+    fontWeight: "bold",
   },
 
- addButton: {
-  height: 50,
-  width: "50%",
-  alignItems: "center",
-  justifyContent: "center",
-  padding: 5,
-  margin: 12,
-  borderRadius: 5,
-  justifyContent: "center", 
-  backgroundColor: "azure", 
-  borderWidth: "3", 
-},
+ 
 
 resetButton: {
   height: 50,
